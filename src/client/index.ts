6d62ec69fcabed450e4c67a035003a555125ecd2
Dajan Vulaj <dajanvulaj@gmail.com>
2026-07-01 23:05:26 +0200

    Update: Languages backend only

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,8 @@
 import { definePluginClient } from '@maxal_studio/kratosjs-react';
 import ProfileEditorBlock from './ProfileEditorBlock';
-import en from './lang/en';
-import sq from './lang/sq';
-
 export default definePluginClient({
 	name: 'profile',
 	blocks: {
 		'profile-editor': ProfileEditorBlock,
 	},
-	translations: { en, sq },
 });
